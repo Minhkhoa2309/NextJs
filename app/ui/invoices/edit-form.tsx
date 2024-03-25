efaf8c138ebd7af3b9ac90d4cba2ba48830fe49e
Minhkhoa2309 <63503994+Minhkhoa2309@users.noreply.github.com>
2024-03-25 10:01:06 +0700

    Edit Card Wrapper edit form

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -50,7 +50,15 @@ export default function EditInvoiceForm({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
-
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.customerId &&
+            state.errors.customerId.map((error: string) => (
+              <p className='mt-2 text-sm text-red-500' key={error}>
+                {error}
+              </p>
+            ))
+          }
+        </div>
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
@@ -71,7 +79,15 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </div>
-
+        <div id="amount-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.amount &&
+            state.errors.amount.map((error: string) => (
+              <p className='mt-2 text-sm text-red-500' key={error}>
+                {error}
+              </p>
+            ))
+          }
+        </div>
         {/* Invoice Status */}
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
@@ -114,6 +130,15 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div id="status-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.status &&
+            state.errors.status.map((error: string) => (
+              <p className='mt-2 text-sm text-red-500' key={error}>
+                {error}
+              </p>
+            ))
+          }
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
